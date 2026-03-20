25a39da0a722d7c6431eec6e9280f978090fd9f4
Yu <50917860+kpryu@users.noreply.github.com>
2026-03-20 16:01:39 +0700

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-let timer;
-window.addEventListener('scroll', function() {
-    document.body.classList.add('scrolling-cursor');
-    
-    clearTimeout(timer);
-    timer = setTimeout(function() {
-        document.body.classList.remove('scrolling-cursor');
-    }, 100);
-});
